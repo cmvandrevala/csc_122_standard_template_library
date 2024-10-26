3096eafb2f2f22d0bb1b2553ff27e9c7882ec914
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2024-10-26 09:47:29 -0500

    Add unordered set notes

---
+++ 5_unordered_set.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unordered_set>
+
+int main()
+{
+  // Unordered Set - A container that stores unique elements in a particular order
+  // See https://cplusplus.com/reference/unordered_set/
+  // See https://en.cppreference.com/w/cpp/container/unordered_set
+
+  unordered_set<int> numbers = {1, 2, 3, 4, 5};
+  // Wait... numbers.push_back(...) does not exist... why?
+  // Wait... numbers.push_front(...) does not exist... why?
+  // Wait... numbers[index] does not exist... why?
+
+  numbers.insert(3);
+  numbers.insert(1);
+  numbers.insert(7);
+  numbers.insert(5);
+  numbers.insert(9);
+
+  cout << "Size: " << numbers.size() << endl;
+
+  for (const int num : numbers)
+  {
+    cout << num << " ";
+  }
+  cout << endl;
+}
